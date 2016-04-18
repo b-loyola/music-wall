f95d3b9c26b6f52a9b1dbb5ead23c61fcc5b9e88
b-silva <berna.loyola@gmail.com>
2016-04-18 18:57:01 +0000

    added new song page

--- app/actions.rb
+++ app/actions.rb
@@ -4,6 +4,20 @@ get '/' do
   erb :index
 end
 
+get '/songs' do
+	redirect '/'
+end
+
 get '/songs/new' do
 	erb :'songs/new'
+end
+
+post '/songs' do
+	@song = Song.new(title: params[:title], author: params[:author], url: params[:url])
+	@song.save ? redirect('/songs') : erb(:'songs/new')
+end
+
+get '/songs/:id' do
+	@song = Song.find(params[:id])
+	erb :'songs/show'
 end
